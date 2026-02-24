1e8307a128d660d37d2f75a4445277d31b5f9532
Mohammed Yazji <mw.alyazji@icloud.com>
2026-02-24 11:37:11 +0200

    feat(auth): implement protect middleware for JWT verification

---
+++ apps/api/src/core/middlewares/authMiddleware.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { promisify } from "util";
+import { config } from "../../config/env";
+import { AppError } from "../errors/AppError";
+import { catchAsync } from "../utils/catchAsync";
+import { UserModel } from "../../modules/users/infrastructure/models/UserModel ";
+
+export const protect = catchAsync(async(req: any, res: Response, next: NextFunction) => {
+    // ENSURE THE TOKEN EXISTS
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
+    } 
+
+    if(!token) {
+        return next(new AppError('Please log in to get access', 401));
+    }
+
+    // ENSURE THE TOKEN IS VALID
+    const decoded: any = jwt.verify(token, config.JWT_SECRET as string);
+
+    // ENSURE THE USER STILL EXISTS
+    const currentUser = await UserModel.findById(decoded.id);
+
+    if(!currentUser) {
+        return next(new AppError('The user belonging to this token does no longer exist', 401));
+    }
+
+    // GRANT ACCESS & STORE THE USER INFORMATION IN THE REQUEST OBJECT TO USE IT IN THE NEXT MIDDLEWARE OR ROUTE HANDLER
+    req.user = currentUser;
+    next();
+
+})
