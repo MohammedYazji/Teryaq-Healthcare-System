df5f23ba651786a67075917ead1f1ade8628fb15
Mohammed Yazji <mw.alyazji@icloud.com>
2026-04-10 14:08:10 +0300

    feat(auth): prevent inactive users to access some features by implement isActive middleware

--- apps/api/src/core/middlewares/authMiddleware.ts
+++ apps/api/src/core/middlewares/authMiddleware.ts
@@ -6,15 +6,19 @@ import { catchAsync } from "../utils/catchAsync";
 import { UserModel } from "../../modules/users/infrastructure/models/UserModel ";
 
 // PROTECT ROUTES
-export const protect = catchAsync(async(req: any, res: Response, next: NextFunction) => {
+export const protect = catchAsync(
+  async (req: any, res: Response, next: NextFunction) => {
     // ENSURE THE TOKEN EXISTS
     let token;
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        token = req.headers.authorization.split(' ')[1];
-    } 
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
 
-    if(!token) {
-        return next(new AppError('Please log in to get access', 401));
+    if (!token) {
+      return next(new AppError("Please log in to get access", 401));
     }
 
     // ENSURE THE TOKEN IS VALID
@@ -23,22 +27,57 @@ export const protect = catchAsync(async(req: any, res: Response, next: NextFunct
     // ENSURE THE USER STILL EXISTS
     const currentUser = await UserModel.findById(decoded.id);
 
-    if(!currentUser) {
-        return next(new AppError('The user belonging to this token does no longer exist', 401));
+    if (!currentUser) {
+      return next(
+        new AppError(
+          "The user belonging to this token does no longer exist",
+          401,
+        ),
+      );
     }
 
     // GRANT ACCESS & STORE THE USER INFORMATION IN THE REQUEST OBJECT TO USE IT IN THE NEXT MIDDLEWARE OR ROUTE HANDLER
     req.user = currentUser;
     next();
-
-})
+  },
+);
 
 // GRANT ACCESS TO SPECIFIC ROLES
 export const restrictTo = (...roles: string[]) => {
-    return (req: any, res: Response, next: NextFunction) => {
-        if(!roles.includes(req.user.role)) {
-            return next(new AppError('You do not have permission to perform this action', 403));
-        }
-        next();
+  return (req: any, res: Response, next: NextFunction) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action", 403),
+      );
     }
-}
+    next();
+  };
+};
+
+// PREVENT INACTIVE USERS TO ACCESS SOME FEATURES
+export const isActive = (req: any, res: Response, next: NextFunction) => {
+  // from "protect" middleware
+  // so should works together
+  const user = req.user;
+
+  if (!user) {
+    return next(new AppError("You are not logged in!", 401));
+  }
+
+  if (user.status === "suspended") {
+    return next(
+      new AppError("Your account has been suspended. Contact support.", 403),
+    );
+  }
+
+  if (user.status === "pending") {
+    return next(
+      new AppError(
+        "Please activate your account via email, or contact support to perform this action.",
+        403,
+      ),
+    );
+  }
+
+  next();
+};
